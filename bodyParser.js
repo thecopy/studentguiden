96c802826a6ae30fbe7ab3afc612a7e3154ed279
Erik Jonsson Thorén <thecopy@gmail.com>
2014-09-17 09:29:43 +0200

    Added exception info

--- bodyParser.js
+++ bodyParser.js
@@ -22,7 +22,7 @@ var parser = function(req, res, next){
         req.body = JSON.parse(body, dateTimeReviver);
         next();
       }catch(e){
-        setError(res, 403, 'Bad Request, illformated JSON');
+        setError(res, 403, 'Bad Request, illformated JSON',e,body);
       }
     }else{
         next();
@@ -32,8 +32,12 @@ var parser = function(req, res, next){
 
 module.exports = parser;
 
-var setError = function(res, code, message){
+var setError = function(res, code, message,e,extra){
   console.log(message);
   res.writeHead(code, message, {'content-type' : 'text/plain'});
+  res.write("Exception:\n");
+  res.write(e.toString());
+  res.write("\n\nJson:\n");
+  res.write(extra)
   res.end(message);
 }
